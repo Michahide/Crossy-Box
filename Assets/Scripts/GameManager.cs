5fea14228cbf39833fdafacf33b7e348aa69b07c
Michahide <73429490+Michahide@users.noreply.github.com>
2022-10-24 08:45:49 +0700

    Masih On Progress

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject grass;
+    [SerializeField] GameObject road;
+    [SerializeField] int extent;
+    [SerializeField] int frontDistance = 10;
+    //seberapa jauh player bisa ke belakang
+    [SerializeField] int minZpos = -5;
+    [SerializeField] int maxSomeTerrainRepeat = 3;
+
+    Dictionary<int, TerrainBlock> map = new Dictionary<int, TerrainBlock>(50);
+
+    private void Start() {
+        //instantiate belakang
+        for (int z = minZpos; z <= 0; z++)
+        {
+            CreateTerrain(grass,z);
+        }
+        
+        //instantiate depan
+        for (int z = 1; z < frontDistance; z++)
+        {   
+            
+            var prefab = GetNextRandomTerrainPrefab(z);
+
+            //instantiate block
+            CreateTerrain(prefab,z);
+        }
+    }
+
+     //method instantiate block
+    private void CreateTerrain(GameObject prefab, int zPos){
+                   
+            var go = Instantiate(prefab, new Vector3(0, 0, zPos), Quaternion.identity);
+            var tb = go.GetComponent<TerrainBlock>();   
+            tb.Build(extent);
+            
+            map.Add(zPos, tb);
+    }
+
+    //method untuk mengatur agar tidak lebih dari n
+    private GameObject GetNextRandomTerrainPrefab(int nextPos){
+        bool isUniform = true;
+        //posisi 1 ke belakang
+        var tbRef = map[nextPos - 1];
+        for (int distance = 2; distance <= maxSomeTerrainRepeat; distance++)
+        {
+            if (map[nextPos - distance].GetType() != tbRef.GetType())
+            {
+                isUniform = false;
+                break;
+            }
+        }
+
+        if(isUniform){
+            if(tbRef is Grass)
+                return road;
+            else
+                return grass;
+        }
+
+        //penentuan terrain block dengan probabilitas 50%
+        return Random.value > 0.5f ? road : grass;
+    }
+}
